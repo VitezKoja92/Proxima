46870e5991cb1dab58a56ec5f5212be41de08a9e
Danilo Krasic <danilo@Danilos-MBP.enmacc.internal>
2017-11-07 12:27:11 +0100

    Register component tested: - checks if the addUser function from UserAPIService is called
    Login component tested:
    - checks if the getAllUsers function from UserAPIService is called
    - still doesn't check the login method as it is not properly implemented

--- src/app/modules/public/components/login/login.component.spec.ts
+++ src/app/modules/public/components/login/login.component.spec.ts
@@ -1,13 +1,33 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { AuthenticationService } from './../../../../services/authentication.service';
+import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { UserAPIService } from './../../../../api/pouchdb-service/user-api.service';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from '../../../material.module';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
+  class UserAPIServiceMock {
+    public getAllUsers() {
+      return {
+        then: () => null
+      };
+    }
+  }
+
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      providers: [
+        { provide: UserAPIService, useClass: UserAPIServiceMock},
+        AuthenticationService
+      ],
+      imports: [ReactiveFormsModule, MaterialModule, RouterTestingModule, BrowserAnimationsModule],
       declarations: [ LoginComponent ]
     })
     .compileComponents();
@@ -16,10 +36,16 @@ describe('LoginComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  // ***** Methods to test: *****
+  // - getUsers() - calls the getAllUsers method from UserAPIService - check if it calls that method
+  // - login(data: any) - provides AuthenticationService with the data and calls its
+  //   login(username, password) method - check if it calls that method
+
+  it('calls the getAllUsers method from UserAPIService', inject([UserAPIService], (UserAPIService: UserAPIService) => {
+    spyOn(UserAPIService, 'getAllUsers').and.callThrough();
+    component.getUsers();
+    expect(UserAPIService.getAllUsers).toHaveBeenCalled();
+  }));
 });
